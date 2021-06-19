42cea0270b148d4c2f22a5579cc5be722bf84dd3
o4six1 <77590500+o4six1@users.noreply.github.com>
2021-06-20 10:29:42 +1200

    Add files via upload

---
+++ CA2/CA2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CA2
+{
+    
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            Console.Write("enter a name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("you matched with Bob, Be nice and only talk in english");
+            Console.WriteLine("hi " + name);
+            string input = Console.ReadLine();
+            if (input == "hi")
+            {
+                Console.WriteLine("die");
+            } else
+            {
+                Console.WriteLine("your dead to me");
+            }
+
+        }
+    }
+}
